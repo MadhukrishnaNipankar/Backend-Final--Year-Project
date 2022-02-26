f43838a1ba8a455f8364a64bfc6c8cfde2660b99
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-02-26 21:50:09 +0530

    added image field

--- README.md
+++
@@ -1,8 +0,0 @@
-# Backend-Final--Year-Project
-
-### 1] USER REGISTRATION - post this data using form 
-##### 1] username
-##### 2] email
-##### 3] password
-##### 4] firstName
-##### 5] lastName
--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# For Managing User Profile Picture
+class UserProfilePhoto(models.Model):
+    sno = models.AutoField(primary_key=True)
+    profile_pic = models.ImageField(null=True,blank=True,upload_to="profilePhotos")
+    user = models.ForeignKey(User,on_delete=models.CASCADE) 
+   
