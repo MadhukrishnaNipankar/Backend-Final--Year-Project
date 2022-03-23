958b1a986e6dd22776ea3c0be0faca92ca136476
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-03-23 23:31:35 +0530

    change

--- Backend/urls.py
+++ Backend/urls.py
@@ -19,9 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from api import views
 
-from django.views.static import serve
-from django.conf.urls import url 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.registerUser),
@@ -38,8 +35,6 @@ urlpatterns = [
     path('addToBookmark/',views.addToBookmark),
     path('getUserHistory/',views.getUserHistory),
     path('getUserBookmark/',views.getUserBookmark),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
     
 
