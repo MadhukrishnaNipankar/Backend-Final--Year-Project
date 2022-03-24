1efacc2cd91c8a1683f7459e65d7788bed04b5a5
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-03-24 19:19:00 +0530

    dj static

--- Backend/settings.py
+++ Backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
@@ -118,11 +119,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 # Default primary key field type
@@ -134,3 +132,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+#latest changes
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
