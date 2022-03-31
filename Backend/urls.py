492f0c6a16696e08fb95ef2debf53b8f388433b3
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-03-31 20:40:52 +0530

    tried to fix media file hosting problem

--- Backend/urls.py
+++ Backend/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from api import views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,8 +36,9 @@ urlpatterns = [
     path('addToBookmark/',views.addToBookmark),
     path('getUserHistory/',views.getUserHistory),
     path('getUserBookmark/',views.getUserBookmark),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
     
 
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
