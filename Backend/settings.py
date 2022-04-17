6981ac276b73e52d5822797861e1abe0a5d008f2
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-04-17 11:55:02 +0530

    added amit bhauncha register

--- Backend/settings.py
+++ Backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hs32d)-y@ncc=4j=tp*%%895*=96^ys@)87c*@rd%vjdz!f1so'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
